1383a85788bc2d310ec0d7e9cfd558d5ec62a7a1
mohammadBaraka <mohammadbarakat342@gmail.com>
2025-04-17 16:41:15 +0300

    for deploy

--- frontend/app/utils/mainUrl.js
+++ frontend/app/utils/mainUrl.js
@@ -1,2 +1,6 @@
-export const mainUrl = "http://localhost:5000/api/v1";
-// export const mainUrl = "https://backend-ecommerce-n337.onrender.com/api/v1";
+// export const mainUrl = ;
+export const mainUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/api/v1"
+    : "https://e-commerce-app-y9k1.onrender.com/api/v1";
+//
